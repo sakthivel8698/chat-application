85e2e7a22c778a362888ee86cb2150447dc75468
sakthivel8698 <sakthivel8698m.s@gmail.com>
2025-02-15 16:58:16 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const cors = require("cors");
+dotenv.config();
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Api working fine");
+});
+
+app.get("/chat", (req, res) => {
+  res.json({ datas: chats, message: "data getting successfully" });
+});
+
+app.get("/chat/:id", async (req, res) => {
+  const queryParams = req?.params?.id;
+  const particularData = chats?.find((item) => item?._id == queryParams);
+  res.json({ data: particularData });
+});
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`)
+);
